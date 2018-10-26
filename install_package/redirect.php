275b38cbb5d694201d74f00f59b93fab2303b67e
mymacbook <1017962047@qq.com>
2018-10-26 16:37:51 +0800

    支付BUG修改

--- install_package/redirect.php
+++ install_package/redirect.php
@@ -5,3 +5,6 @@
  * Date: 2018/10/16
  * Time: 上午10:37
  */
+define('PHPCMS_PATH', dirname(__FILE__).DIRECTORY_SEPARATOR);
+include PHPCMS_PATH.'phpcms/base.php';
+header("Location: ".APP_PATH.'index.php?m=member&c=index');
